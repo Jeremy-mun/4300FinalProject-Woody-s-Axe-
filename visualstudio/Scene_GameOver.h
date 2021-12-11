e24dd6cee693e2a892d2b1509be2fe4c0371d0c0
jp6337 <jp6337@mun.ca>
2021-12-11 20:21:18 -0330

    commit

---
+++ visualstudio/Scene_GameOver.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Common.h"
+#include "Scene.h"
+#include <map>
+#include <memory>
+#include <deque>
+
+#include "EntityManager.h"
+
+class Scene_GameOver : public Scene
+{
+
+};
